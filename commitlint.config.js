163d4bb0ebb49368d4efb3dd6b043d2343077c80
Colin Prokopowicz <colin.proko@c1cloud.ovh>
2024-06-07 14:52:27 +0200

    fix: test

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,3 +1,22 @@
-module.exports = {
+export default {
   extends: ["@commitlint/config-angular"],
+  rules: {
+    "type-enum": [
+      2,
+      "always",
+      [
+        "chore",
+        "build",
+        "ci",
+        "docs",
+        "feat",
+        "fix",
+        "perf",
+        "refactor",
+        "revert",
+        "style",
+        "test",
+      ],
+    ],
+  },
 }
